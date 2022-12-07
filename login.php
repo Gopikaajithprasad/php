fd059ffc8686296ffa90d254c8a756b37e45a4d6
Gopikaajithprasad <116332109+Gopikaajithprasad@users.noreply.github.com>
2022-12-06 18:52:38 -0800

    Add files via upload

---
+++ login.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Login</title>
+    </head>
+    <body>
+        <h1>WELCOME</h1>
+        <a href="index1.php">Logout</a>
+    </body>
+</html>
